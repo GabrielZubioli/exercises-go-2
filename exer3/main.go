7037ea28371cdf854e911676b0ad2c5b36ab709a
Gabriel Zubioli <zubioligabriel@gmail.com>
2025-05-25 21:47:24 -0300

    exercise 3 finished

--- exer3/main.go
+++ exer3/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sort"
+)
 
-func main(){
-	
+func main() {
+    var numeros [12]int
+
+    for i := 0; i < len(numeros); i++ {
+        fmt.Printf("Digite o %d° numero: ", i+1)
+        fmt.Scanln(&numeros[i])
+    }
+
+    numerosSlice := numeros[:] 
+    sort.Ints(numerosSlice)
+
+    fmt.Println("Números ordenados:", numerosSlice)
 }
